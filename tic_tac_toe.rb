61a7593edaea5c7052f0ed0e3de927c4ee1c2dd4
samanthaahannah <samanthaahannah@outlook.com>
2025-06-27 14:43:28 +0100

    Add input for board + swap player markers

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,74 +1,49 @@
-
 board =[[' ', ' ', ' '],[ ' ', ' ', ' '],[ ' ', ' ', ' ']]
 
 class Player
-  
-end
 
-class Computer < Player
-  def place_marker(board)
-    joined_arr = board.flatten
-    if joined_arr.all?(" ")
-      joined_arr[0] = "X"
-      joined_arr.shuffle!.each_slice(3).to_a
-    else
-      board.find{|(x, y)| }
-    end
+  def initialize(marker)
+    @marker = marker
   end
-end
 
-class Human < Player
   def place_marker(board)
     p "place marker"
     input = gets.chomp.to_i
-    case input >= 1 && input <= 9 && board.flatten[input-1] == " "
-    when input == 1 
-      board[0][0] = "O"
-      return board
-    when input == 2 
-      board[0][1] = "O"
-      return board
-    when input == 3
-      board[0][2] = "O"
-      return board
-    when input == 4 
-      board[1][0] = "O"
-      return board
-    when input == 5
-      board[1][1] = "O"
-      return board
-    when input == 6
-      board[1][2] = "O"
-      return board
-    when input == 7
-      board[2][0] = "O"
-      return board
-    when input == 8
-      board[2][1] = "O"
-      return board
-    when input == 9
-      board[2][2] = "O" 
-      return board
+    if input > 0 && input < 4
+        board[0][input - 1] = @marker
+    elsif input >= 4 && input < 7
+        board[1][input - 4] = @marker
+    elsif input >= 7 && input < 10
+        board[2][input - 7] = @marker
     else
-      p "Wrong input. Try again."
-      place_marker(board)
+      p "Invalid input: try again!"
+        place_marker(board)
     end
   end
 end
 
-def draw_board(board)
-  puts "                                         "
-  puts " #{board[0][0]} | #{board[0][1]} | #{board[0][2]} "
-  puts "-----------"
-  puts " #{board[1][0]} | #{board[1][1]} | #{board[1][2]} "
-  puts "-----------"
-  puts " #{board[2][0]} | #{board[2][1]} | #{board[2][2]} "
+class Board
+  def draw_board(board)
+    puts "                                         "
+    puts " #{board[0][0]} | #{board[0][1]} | #{board[0][2]} "
+    puts "-----------"
+    puts " #{board[1][0]} | #{board[1][1]} | #{board[1][2]} "
+    puts "-----------"
+    puts " #{board[2][0]} | #{board[2][1]} | #{board[2][2]} "
+  end
 end
 
-draw_board(board)
-cpu = Computer.new
-board = cpu.place_marker(board)
-draw_board(board)
-human = Human.new
-human.place_marker(board)
-draw_board(board)
+def play_game(board)
+  main_board = Board.new
+  main_board.draw_board(board)
+  player1 = Player.new("X")
+  player2 = Player.new("O")
+  2.times do
+    p player1.place_marker(board)
+    main_board.draw_board(board)
+    p player2.place_marker(board)
+    main_board.draw_board(board)
+  end
+end
+
+play_game(board)
